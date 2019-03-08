2cb72beae2ab2ac758f31d5d6605db063a05e012
Emma Dayton <emma.dayton85@gmail.com>
2019-03-08 11:21:59 -0800

    finish python logic and add variables to index

--- rps.py
+++ rps.py
@@ -1,20 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from random import choice
 app = Flask(__name__)
 
 rps_dict = {
-            'rock': {'paper': 'lose', 'scissors': 'win', 'rock': 'tie'},
-            'paper': {'paper': 'tie', 'scissors': 'lose', 'rock': 'win'},
-            'scissors': {'paper': 'win', 'scissors': 'tie', 'rock': 'lose'}
+            'rock': {'paper': 'loses', 'scissors': 'wins', 'rock': 'ties'},
+            'paper': {'paper': 'ties', 'scissors': 'loses', 'rock': 'wins'},
+            'scissors': {'paper': 'wins', 'scissors': 'ties', 'rock': 'loses'}
 }
+
+computer = ['rock', 'paper', 'scissors']
+
+
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
+@app.route('/result', methods=['POST', 'GET'])
+def start():
+    winner = ''
+    player = ''
+    comp = choice(computer)
+    if request.form['my_options'] == 'rock':
+        player = 'rock'
+    elif request.form['my_options'] == 'paper':
+        player = 'paper'
+    elif request.form['my_options'] == 'scissors':
+        player = 'scissors'
+    else:
+        return render_template('index.html')
+    comp_str = f'Computer chooses: {comp}.'
+    play_str = f'Player chooses: {player}.'
+    resolution = f'Player {rps_dict[player][comp]}.'
 
-
-
+    return render_template('index.html', comp_str=comp_str, play_str=play_str, resolution=resolution)
 
 
 
